a9e6df6dd6e3eb5a7dce7fc7f44f114074c2f61e
sangseunglee <lss9209>
2021-01-16 20:10:13 +0900

    Project of JPA and SpringBoot Day 6

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -7,10 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JpashopApplication {
 
 	public static void main(String[] args) {
-
-		Hello hello = new Hello();
-		hello.setStr("메롱메롱");
-		System.out.println(hello.getStr());
 		SpringApplication.run(JpashopApplication.class, args);
 	}
 
